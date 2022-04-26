6a4a3d593899a8a49eed3aa38d76f31822a1ec3a
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-26 10:27:44 -0600

    Test: can list food trucks that sell a specific item

--- spec/2iteration_spec.rb
+++ spec/2iteration_spec.rb
@@ -44,5 +44,19 @@ describe "Iteration 2" do
 
       expect(event.food_truck_names).to eq ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     end
+
+    it "can list food trucks that sell a specific item" do
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.food_trucks_that_sell(item1)).to eq [food_truck1, food_truck3]
+    end
+
   end
 end
