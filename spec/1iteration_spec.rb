70b4b0fc1821cad843501f33b1ce354b066b3367
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-26 09:22:59 -0600

    Test: describe foodtruck exists and has attributes

--- spec/1iteration_spec.rb
+++ spec/1iteration_spec.rb
@@ -1,16 +1,33 @@
 require './lib/item'
 require './lib/food_truck'
+# require './lib/event'
 
-describe Item do
-
+describe "Iteration 1" do
   let(:item1) { Item.new({name: 'Peach Pie (Slice)', price: "$3.75"}) }
   let(:item2) { Item.new({name: 'Apple Pie (Slice)', price: '$2.50'}) }
+  let(:food_truck) { FoodTruck.new("Rocky Mountain Pies") }
 
-  it "exists and has attributes" do
-    expect(item1).to be_a Item
-    expect(item2).to be_a Item
-    expect(item2.name).to eq("Apple Pie (Slice)")
-    expect(item2.price).to eq(2.50)
+
+  describe Item do
+    it "exists and has attributes" do
+      expect(item1).to be_a Item
+      expect(item2).to be_a Item
+      expect(item2.name).to eq("Apple Pie (Slice)")
+      expect(item2.price).to eq(2.50)
+    end
   end
 
+  describe FoodTruck do
+    it "exists and has attributes" do
+      expect(food_truck).to be_a FoodTruck
+      expect(food_truck.name).to eq("Rocky Mountain Pies")
+      expect(food_truck.inventory).to eq({})
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+
+    it "can stock items" do
+      food_truck.stock(item1, 30)
+      expect(food_truck.inventory).to eq({ item1 => 30 })
+    end
+  end
 end
